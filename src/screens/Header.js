cf7502f615f9a8b4cc4e716a725b42ed91b6494c
Sonali2267 <shamumehra988@gmail.com>
2023-09-08 12:51:44 +0530

    in this file add link for home,about,contact,support,employee

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,27 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <Link to="/home" className='nav-link'>Home</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <Link to="/about" className='nav-link'>About</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <Link to="/contact" className='nav-link'>Contact</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <Link to="/support" className='nav-link'>Support</Link>
+        </li>
+        <li class="nav-item">
+          <Link to="/employee" className='nav-link'>Employee</Link>
         </li>
       </ul>
     </div>
