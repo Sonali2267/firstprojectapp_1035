fe6815ce538e58bcab552c2e319287cac0bc32d7
Sonali2267 <shamumehra988@gmail.com>
2023-09-08 11:38:19 +0530

    react start pages create route set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
+import About from './screens/About';
+import Contact from './screens/Contact';
+import Support from './screens/Support';
+import Employee from './screens/Employee';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <BrowserRouter>
+      <Routes>
+        <Route path='home' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='support' element={<Support/>}/>
+        <Route path='employee' element={<Employee/>}/>
+
+      </Routes>
+      </BrowserRouter>
+     {/* <Home/>
+     <About/>
+     <Contact/>
+     <Support/>
+     <Employee/> */}
+    </div>);
 }
 
 export default App;
---
+++ src/screens/Employee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Employee() {
+  return (
+    <div className='text-info text-center'>Employee Page</div>
+  )
+}
+
+export default Employee
