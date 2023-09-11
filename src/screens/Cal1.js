448c6131bc1d31ad486dee3da9d636a81b813504
Sonali2267 <shamumehra988@gmail.com>
2023-09-11 13:46:12 +0530

    add function declare or value set in textbox

--- src/screens/Cal1.js
+++ src/screens/Cal1.js
@@ -12,16 +12,29 @@ function Cal1() {
         setData({...data,[event.target.name]:Number(event.target.value)});
         console.log(data);
     };
+
+    const sumClick=()=>{
+        setData({...data,res:data.num1+data.num2});
+    };
+    const mulClick=()=>{
+        setData({...data,res:data.num1*data.num2});
+    };
+     const divClick=()=>{
+        setData({...data,res:data.num1/data.num2});
+    };
+    const clearClick=()=>{
+        setData({num1:0,num2:0,res:0});
+    };
   return (
     <div>
         <b>Enter First Number:</b>
-        <input type='number' name='num1' onChange={changeHandler}/><br/>
-        <input type='number' name='num2' onChange={changeHandler}/><br/>
-        <b>Result:</b><br/>
-        <button className='btn btn-info'>Sum</button>
-        <button className='btn btn-primary'>Mul</button>
-        <button className='btn btn-success'>Div</button>
-        <button className='btn btn-danger'>Clear</button>
+        <input value={data.num1} type='number' name='num1' onChange={changeHandler}/><br/>
+        <input value={data.num2} type='number' name='num2' onChange={changeHandler}/><br/>
+        <b>Result:</b>{data.res}<br/>
+        <button onClick={sumClick} className='btn btn-info'>Sum</button>
+        <button onClick={mulClick} className='btn btn-primary'>Mul</button>
+        <button onClick={divClick} className='btn btn-success'>Div</button>
+        <button onClick={clearclick} className='btn btn-danger'>Clear</button>
     </div>
   )
 }
