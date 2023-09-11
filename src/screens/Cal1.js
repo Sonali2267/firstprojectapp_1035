07b94a373bfaf2d1baab1deed273685cb805c5c8
Sonali2267 <shamumehra988@gmail.com>
2023-09-11 13:19:03 +0530

    cal1.js -  create cal. and then define by variable key or value

--- src/screens/Cal1.js
+++ src/screens/Cal1.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 function Cal1() {
+
+    const [data,setData]=useState({
+        num1:0,
+        num2:0,
+        res:0,
+    })
   return (
-    <div>Cal1</div>
+    <div>
+        <b>Enter First Number:</b>
+        <input type='number' name='num1'/><br/>
+        <input type='number' name='num2'/><br/>
+        <b>Result:</b><br/>
+        <button className='btn btn-info'>Sum</button>
+        <button className='btn btn-primary'>Mul</button>
+        <button className='btn btn-success'>Div</button>
+        <button className='btn btn-danger'>Clear</button>
+    </div>
   )
 }
 
